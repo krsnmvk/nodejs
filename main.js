eb79101e5fa55b327ac3d7bcd2907ef41c20e01b
Krisno Mukti <krsnmvk@gmail.com>
2025-05-10 20:27:44 +0000

    05 parsing request bodies

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { writeFileSync } = require('node:fs');
+const { writeFile } = require('node:fs/promises');
 const http = require('node:http');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write(
@@ -13,16 +14,35 @@ const server = http.createServer((req, res) => {
   }
 
   if (req.url === '/message' && req.method === 'POST') {
-    writeFileSync('message.txt', 'dummy');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    const body = [];
+
+    req.on('data', (chunk) => body.push(chunk));
+
+    req.on('end', async () => {
+      try {
+        const parseBody = Buffer.concat(body).toString();
+        const message = decodeURIComponent(
+          parseBody.split('=')[1].replace(/\+/, ' ')
+        );
+
+        await writeFile('message.txt', message);
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      } catch (err) {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    });
+
+    return;
   }
 
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
   res.write('<html>');
   res.write('<head><title>First Page</title></head>');
-  res.write('<body><h1>hello from my nodejs server</h1></body>');
+  res.write('<body><h1>Hello from my Node.js server</h1></body>');
   res.write('</html>');
   res.end();
 });
