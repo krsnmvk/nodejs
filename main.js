a070809de9af34d5cf37350cea24ea561140728f
Krisno Mukti <krsnmvk@gmail.com>
2025-05-10 21:48:03 +0000

    07 adding a 404 error page

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoute);
 app.use(shopRoute);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found!</h1>');
+});
+
 app.listen(8080, () => {
   console.log('Server running on: http://localhost:8080');
 });
--- routes/shop.route.js
+++ routes/shop.route.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 
 const shopRoute = Router();
 
-shopRoute.use('/', (req, res, next) => {
+shopRoute.get('/', (req, res, next) => {
   res.send('<h1>hello from express</h1>');
 });
 
