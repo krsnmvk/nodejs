0792e2dee696a66efcff7d7dd67bf10ed7b85962
Krisno Mukti <krsnmvk@gmail.com>
2025-05-10 21:23:04 +0000

    02 adding middleware

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('in the middleware');
+
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('in another middleware');
+});
+
 app.listen(8080, () => {
   console.log('Server running on: http://localhost:8080');
 });
