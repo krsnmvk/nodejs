3c6724411903137c72e6c4c30e2cf3ea9028e62f
Krisno Mukti <krsnmvk@gmail.com>
2025-05-10 19:58:12 +0000

    02 sending responses

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
-  console.log('hello nedejs');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Page</title></head>');
+  res.write('<body><h1>hello from my nodejs server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(8080, () => {
