f84235461af22380daece1407d8ef5d3e8c0f64f
Krisno Mukti <krsnmvk@gmail.com>
2025-05-10 20:33:59 +0000

    06 using the node modules system

--- main.js
+++ main.js
@@ -1,51 +1,7 @@
-const { writeFile } = require('node:fs/promises');
 const http = require('node:http');
+const requestHandler = require('./routes');
 
-const server = http.createServer(async (req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write(
-      '<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (req.url === '/message' && req.method === 'POST') {
-    const body = [];
-
-    req.on('data', (chunk) => body.push(chunk));
-
-    req.on('end', async () => {
-      try {
-        const parseBody = Buffer.concat(body).toString();
-        const message = decodeURIComponent(
-          parseBody.split('=')[1].replace(/\+/, ' ')
-        );
-
-        await writeFile('message.txt', message);
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      } catch (err) {
-        res.statusCode = 500;
-        res.end('Internal Server Error');
-      }
-    });
-
-    return;
-  }
-
-  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
-  res.write('<html>');
-  res.write('<head><title>First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(requestHandler);
 
 server.listen(8080, () => {
   console.log('Server running on: http://localhost:8080');
