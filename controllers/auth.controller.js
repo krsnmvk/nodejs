60e63f7397012fab5bb47a46fc3e243d1346963d
Krisno Mukti <krsnmvk@gmail.com>
2025-05-16 17:58:17 +0700

    04: setting a cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,17 @@
 export function getLogin(req, res, next) {
+  const isLoggedIn = req.get('Cookie').split('=')[1];
+
   return res.render('auth/login', {
     title: 'Login',
     href: '/login',
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 }
 
 export function postLogin(req, res, next) {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'isLoggedIn=true');
+  // console.log(res);
+  // console.log(req);
 
   return res.redirect('/');
 }
